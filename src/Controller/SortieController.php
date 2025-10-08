7f0afc9fb0bdc1381e0ad2599193795da0a2e37a
Corentin COTTEREAU <109530954+Corentin-cott@users.noreply.github.com>
2025-10-08 11:36:13 +0200

    Création des éléments de sorties (Controller, type de formulaire & formulaire)

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SortieController extends AbstractController
+{
+    #[Route('/sortie/creer', name: 'app_sortie')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $sortie = new Sortie();
+
+        $form = $this->createForm(SortieType::class, $sortie);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($sortie);
+            $em->flush();
+
+            $this->addFlash('success', 'Sortie created successfully!');
+        }
+
+        return $this->render('sorties/creer.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
