b6bdfe689f9b2fad33d42799caab9cfc4a74da54
Corentin COTTEREAU <109530954+Corentin-cott@users.noreply.github.com>
2025-10-17 11:23:34 +0200

    Ajout .dockerignore & modification configuration docker

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+git pull
 docker compose down
+rm -rf var/cache/*
 docker compose build
 docker compose up -d
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,8 @@ php bin/console asset-map:compile
 chown -R www-data:www-data /var/www/html/var /var/www/html/public
 chmod -R 775 /var/www/html/var /var/www/html/public
 
+# Lance le scheduler
+php bin/console messenger:consume scheduler_hello -vv --env=prod &
+
 # Lance la commande de lancement du contenaire
 exec "$@"
